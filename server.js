1ca8a30d760e0c13debf31c81f4eb9b1abeebde9
jsoft-dev <64036527+jsoft-dev@users.noreply.github.com>
2020-11-06 14:33:46 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var ser = http.createServer(function (req, res) {
+    // res.writeHead(200,{'Content-Type':'text/plain'});
+    res.write("ADDOOOOO WEDDDAAAA.....");
+    res.end();
+});
+ser.listen(80);
