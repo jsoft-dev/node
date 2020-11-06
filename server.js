c9a97af69a9c0c983d31f16e55fe0c9b591ee38b
jsoft-dev <64036527+jsoft-dev@users.noreply.github.com>
2020-11-06 14:40:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var ser = http.createServer(function (req, res) {
-    // res.writeHead(200,{'Content-Type':'text/plain'});
+    res.writeHead(200,{'Content-Type':'text/plain'});
     res.write("ADDOOOOO WEDDDAAAA.....");
     res.end();
 });
